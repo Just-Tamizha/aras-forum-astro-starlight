18dbe9a37a267ce651bac00c4f720d749a5b3316
Tamizha <85663828+Just-Tamizha@users.noreply.github.com>
2026-05-30 20:55:12 +0530

    Configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://Just-Tamizha.github.io',
+	base: '/aras-forum-astro-starlight',
 	integrations: [
 		starlight({
 			title: 'Tamizha',
